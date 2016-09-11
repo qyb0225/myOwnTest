cddc18869cbb51ea377a9bc503965a9003bf10b9
unknown <>
2016-09-11 22:54:06 +0800

    operation--下拉单击问题

--- tool/ControlorJs.php
+++ tool/ControlorJs.php
@@ -342,7 +342,7 @@ function OperationJsPost() {
             $('input[type=text]').blur(function(){ setTimeout(()=>{ 
                 $(this).siblings('.ds-checkbox-items').hide();
                 $(this).removeAttr('keydown');
-                }, 100);});
+                }, 200);});
             $('input[type=text]').not('#operation66').change(function() {
                  var countValue = 0;
                  for(var key in countInputObj ) {
